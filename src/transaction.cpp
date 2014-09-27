c383440ed04590c770f45b6bef5e9d207f3f6d9a
James Goode <james@uwcs.co.uk>
2014-09-27 09:12:47 +0100

    Fix transactions again?

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,6 +1,7 @@
+#define HADES_ENABLE_DEBUGGING
+
 #include "hades/transaction.hpp"
 
-#include <cassert>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
@@ -21,13 +22,22 @@ hades::transaction::transaction(
 {
     // Start a SQLite TRANSACTION if outside of a transaction.
     if(m_connection.transaction_depth() == 0)
+    {
         devoid("BEGIN TRANSACTION", empty_row(), m_connection);
-    else
-        devoid(
-                mkstr() << "SAVEPOINT " << m_savepoint_name,
-                empty_row(),
-                m_connection
-                );
+#ifdef HADES_ENABLE_DEBUGGING
+        std::cerr << "BEGIN TRANSACTION" << std::endl;
+#endif
+    }
+
+    devoid(
+            mkstr() << "SAVEPOINT " << m_savepoint_name,
+            empty_row(),
+            m_connection
+            );
+#ifdef HADES_ENABLE_DEBUGGING
+    std::cerr << "SAVEPOINT " << m_savepoint_name << std::endl;
+#endif
+
     m_connection.enter_transaction(this);
 }
 
@@ -36,18 +46,25 @@ void hades::transaction::commit()
     if(m_released)
         throw std::runtime_error("Savepoint already released");
 
+    devoid(
+        mkstr() << "RELEASE SAVEPOINT " << m_savepoint_name,
+        empty_row(),
+        m_connection
+        );
+#ifdef HADES_ENABLE_DEBUGGING
+    std::cerr << "RELEASE SAVEPOINT " << m_savepoint_name << std::endl;
+#endif
+
     m_connection.finish_transaction();
+    m_released = true;
 
     if(m_connection.transaction_depth() == 0)
+    {
         devoid("COMMIT TRANSACTION", empty_row(), m_connection);
-    else
-        devoid(
-            mkstr() << "RELEASE SAVEPOINT " << m_savepoint_name,
-            empty_row(),
-            m_connection
-            );
-
-    m_released = true;
+#ifdef HADES_ENABLE_DEBUGGING
+        std::cerr << "COMMIT TRANSACTION" << std::endl;
+#endif
+    }
 }
 
 void hades::transaction::rollback()
@@ -55,20 +72,30 @@ void hades::transaction::rollback()
     if(m_released)
         throw std::runtime_error("Savepoint already released");
 
-    if(m_connection.transaction_depth() < 2)
+    if(m_connection.transaction_depth() > 1)
     {
-        assert(m_connection.transaction_depth() == 1);
-        devoid("ROLLBACK TRANSACTION", empty_row(), m_connection);
-    }
-    else
         devoid(
             mkstr() << "ROLLBACK TO SAVEPOINT " <<
                 m_connection.peek2_transaction()->m_savepoint_name,
             empty_row(),
             m_connection
             );
+#ifdef HADES_ENABLE_DEBUGGING
+        std::cerr << "ROLLBACK TO SAVEPOINT " <<
+            m_connection.peek2_transaction()->m_savepoint_name << std::endl;
+#endif
+    }
 
     m_connection.finish_transaction();
+    m_released = true;
+
+    if(m_connection.transaction_depth() == 0)
+    {
+        devoid("ROLLBACK TRANSACTION", empty_row(), m_connection);
+#ifdef HADES_ENABLE_DEBUGGING
+        std::cerr << "ROLLBACK TRANSACTION" << std::endl;
+#endif
+    }
 }
 
 hades::transaction::~transaction()
