4cd053824c35c31e0f4bc1fb13f9993ae23a939e
James Goode <james@uwcs.co.uk>
2014-10-24 13:08:00 +0100

    attribute_list::extend will not repeat attributes.

--- include/hades/attribute_list.hpp
+++ include/hades/attribute_list.hpp
@@ -1,6 +1,8 @@
 #ifndef HADES_ATTRIBUTE_LIST_HPP
 #define HADES_ATTRIBUTE_LIST_HPP
 
+#include <type_traits>
+
 #include "hades/bind_values.hpp"
 #include "hades/get_column.hpp"
 #include "styx/object_accessor.hpp"
@@ -13,8 +15,17 @@ namespace hades
     template<const char *...Attributes>
     class tuple;
 
+    namespace detail
+    {
+        template<const char *Attr>
+        struct attribute_list_member
+        {
+        };
+    }
+
     template<const char *...Attributes>
-    class attribute_list
+    class attribute_list :
+        public detail::attribute_list_member<Attributes>...
     {
     public:
         /*!
@@ -22,7 +33,14 @@ namespace hades
          * list of attributes.
          */
         template<const char *Attribute>
-        using extend = attribute_list<Attributes..., Attribute>;
+        using extend = typename std::conditional<
+            std::is_base_of<
+                detail::attribute_list_member<Attribute>,
+                attribute_list<Attributes...>
+                >::value,
+            attribute_list<Attributes...>,
+            attribute_list<Attributes..., Attribute>
+            >::type;
 
         /*!
          * \brief A tuple type with the same attributes.
